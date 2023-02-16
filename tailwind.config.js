b66d6a63141458d64dd80dd2b058156110aadf16
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-02-16 19:45:12 -0300

    refactor(global): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require("tailwindcss/colors")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*{html,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    }
   },
-  plugins: [],
+  plugins: []
 }
