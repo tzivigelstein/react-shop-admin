d5330cc257abf0b634e2c796363f20b0bf91c5fe
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-02-18 16:46:07 -0300

    feat(login): implement logic in login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,36 @@
+import { FormEvent, useState } from 'react'
 import { LockClosedIcon } from '@heroicons/react/solid'
+import useAuth from '@/hooks/useAuth'
+
+interface Data {
+  email: string
+  password: string
+}
 
 export default function LoginPage() {
+  const { user, login } = useAuth()
+  const [data, setData] = useState<Data>({
+    email: '',
+    password: ''
+  })
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    login(data)
+
+    console.log(user)
+  }
+
+  function handleChange(event: FormEvent<HTMLInputElement>) {
+    const current = event.currentTarget
+
+    setData((prev) => ({
+      ...prev,
+      [current.name]: current.value
+    }))
+  }
+
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -15,7 +45,7 @@ export default function LoginPage() {
               Sign in to your account
             </h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -26,10 +56,11 @@ export default function LoginPage() {
                   id="email-address"
                   name="email"
                   type="email"
-                  autoComplete="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  value={data.email}
+                  onChange={handleChange}
                 />
               </div>
               <div>
@@ -40,10 +71,11 @@ export default function LoginPage() {
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="current-password"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  value={data.password}
+                  onChange={handleChange}
                 />
               </div>
             </div>
