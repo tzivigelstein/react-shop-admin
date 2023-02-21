a78ab0cd972fa2df7769bd5ad9c340fd57b3ac62
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-02-21 16:09:37 -0300

    feat(context): update types for loading an error

--- src/hooks/AuthContext.ts
+++ src/hooks/AuthContext.ts
@@ -1,16 +1,21 @@
 import services from '@/services'
 import { IUser } from '@/services/types'
 import { createContext } from 'react'
+import { IError } from '@/hooks/useAuth'
 
 type Login = typeof services.auth.login
 export type LoginParameters = Parameters<Login>
 
 interface IAuthContext {
   user: IUser | null
+  loading: boolean
+  error: IError | null
   login: (...args: LoginParameters) => void
 }
 
 export default createContext<IAuthContext>({
   user: null,
+  loading: false,
+  error: null,
   login: ({ email, password }) => Promise<void>
 })
