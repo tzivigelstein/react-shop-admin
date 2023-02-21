22324eb781d3edc3f384dd71d920b66394cb4bc0
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-02-21 16:15:42 -0300

    refactor(utils): create class for data fetching

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,19 +1,23 @@
-export async function apiFetch(URL: string, config: RequestInit = {}) {
-  return await fetch(URL, {
-    headers: {
-      'Content-Type': 'application/json',
-      ...config?.headers
-    },
-    ...config
-  })
-    .then((response) => {
+class Fetching {
+  headers: { [key: string]: string } = {
+    'Content-Type': 'application/json'
+  }
+
+  async fetch(URL: string, config: RequestInit = {}) {
+    return await fetch(URL, {
+      headers: {
+        ...this.headers,
+        ...config?.headers
+      },
+      ...config
+    }).then((response) => {
       if (response.ok) {
         return response.json()
       } else {
         throw new Error(response.statusText)
       }
     })
-    .catch((error) => {
-      console.log(error)
-    })
+  }
 }
+
+export default new Fetching()
