88d52519e7f8a50609b8e78d1f87098cc4bc7d75
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-02-21 16:16:44 -0300

    feat(hooks): implement cookies, loading, and error

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import Fetching from '@/services/utils'
 import { useContext, useState } from 'react'
 import Cookie from 'js-cookie'
 import { IUser } from '@/services/types'
@@ -8,17 +9,50 @@ export default function useAuth() {
   return useContext(AuthContext)
 }
 
+export interface IError {
+  name: string
+  message: string
+}
+
 export function useAuthProvider() {
   const [user, setUser] = useState<IUser | null>(null)
+  const [error, setError] = useState<IError | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const handleError = (error: Error) =>
+    setError({
+      name: error.name,
+      message: error.message
+    })
 
   const login = async (...data: LoginParameters) => {
-    const user = await services.auth.login(...data)
+    setLoading(true)
+    const tokens = await services.auth
+      .login(...data)
+      .catch(handleError)
+      .finally(() => setLoading(false))
 
-    setUser(user)
+    if (tokens) {
+      const { access_token } = tokens
+      Cookie.set('token', access_token, { expires: 5 })
+
+      Fetching.headers['Authorization'] = `Bearer ${access_token}`
+    }
+
+    setLoading(true)
+
+    const user = await services.auth
+      .getProfile()
+      .catch(handleError)
+      .finally(() => setLoading(false))
+
+    if (user) setUser(user)
   }
 
   return {
     user,
+    loading,
+    error,
     login
   }
 }
